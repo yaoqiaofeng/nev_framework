0c787040083d8b577035de1c61948226d4bb43f9
乔什么 <yaoqiaofeng@sohu.com>
2018-06-14 10:58:42 +0800

    2018-06-14

---
+++ tools/数据模型/nModel.dpr
@@ -0,0 +1,14 @@
+program nModel;
+
+uses
+  Forms,
+  nMainForm in 'nMainForm.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
